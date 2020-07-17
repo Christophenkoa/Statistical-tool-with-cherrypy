a262574e6f069c579b5b4398f059fa9b4423bce2
SONFACKZ <elfridsonfack@gmail.com>
2020-07-17 10:55:19 +0100

    Little Update: I Parse the output to json in terms to decode the matrix of covariance and Correlation coefficient.

--- cherrypy/tutorial/tut01_helloworld.py
+++ cherrypy/tutorial/tut01_helloworld.py
@@ -14,7 +14,11 @@ import statistics
 
 
 # use of numpy.cov 
-import numpy as np 
+import numpy as np
+import json
+import pandas as pd
+import seaborn as sn
+import matplotlib.pyplot as plt
   
 
 
@@ -24,45 +28,56 @@ class HelloWorld:
 
     # Expose the index method through the web. CherryPy will never
     # publish methods that don't have the exposed attribute set to True.
-    #@cherrypy.expose
-    #def index(self):
-        # CherryPy will call this method for the root URI ("/") and send
-        # its return value to the client. Because this is tutorial
-        # lesson number 01, we'll just send something really simple.
-        # How about...
-        #return 'Hello world!'
-    @cherrypy.expose
-    def index(self):
-        # Creating a sample of data 
-        sample = [2.74, 1.23, 2.63, 2.22, 3, 1.98]
-        # Function will automatically calculate 
-        # it's mean and set it as xbar 
-        return("Variance of sample set is % s" 
-      %(statistics.variance(sample)))
-    
-    @cherrypy.expose
-    def index(self):
-        # creating a simple data - set 
-        sample = [1, 2, 3, 4, 5] 
-  
-        # Prints standard deviation 
-        # xbar is set to default value of 1 
-        return("Standard Deviation of sample is % s " 
-                % (statistics.stdev(sample)))
 
-    @cherrypy.expose
-    def index(self):
-        x = np.array([[0, 3, 4], [1, 2, 4], [3, 4, 5]])
-        # print("Shape of array:\n", np.shape(x)) 
-        return("Covarinace matrix of x: \n", np.cov(x))
+
+
+#To run the code, uncomment each method and run to test if it's works.
+#First Uncomment the index method for the variance
+#After test comment again and uncoment the next and so on.
+
+
+    # @cherrypy.expose
+    # def index(self):
+    #     # Creating a sample of data 
+    #     sample = [2.74, 1.23, 2.63, 2.22, 3, 1.98]
+    #     # Function will automatically calculate 
+    #     # it's mean and set it as xbar 
+    #     return("Variance of sample set is % s" 
+    #   %(statistics.variance(sample)))
     
-    @cherrypy.expose
-    def Correlation(self):
-        a = [1,4,6]
-        b = [1,2,3] 
-        #Correlation
-        corrmat = np.corrcoef(a,b)
-        print("The correlation coeficient is: "+ corrmat)
+    # @cherrypy.expose
+    # def index(self):
+    #     # creating a simple data - set 
+    #     sample = [1, 2, 3, 4, 5] 
+  
+    #     # Prints standard deviation 
+    #     # xbar is set to default value of 1 
+    #     return("Standard Deviation of sample is % s " 
+    #             % (statistics.stdev(sample)))
+
+    # @cherrypy.expose
+    # def index(self):
+    #     a = [1, 4, 6]
+    #     b = [1, 2, 3] 
+
+    #     data = np.array([a, b])
+    #     covMatrix = np.cov(data,bias=False)
+    #     correlation = np.corrcoef(a,b)
+    #     df = pd.DataFrame(covMatrix, index=['row_1', 'row_2'], columns=['col_1', 'col_2'])
+    #     x = df.to_json(orient='index')
+    #     return("Covariance matrix of x: \n", x)
+
+    
+    # @cherrypy.expose
+    # def index(self):
+    #     a = [1, 4, 6]
+    #     b = [1, 2, 3] 
+    #     #Correlation
+    #     correlation = np.corrcoef(a,b)
+    #     df = pd.DataFrame(correlation, index=['row_1', 'row_2'], columns=['col_1', 'col_2'])
+    #     x = df.to_json(orient='index')
+
+    #     return("The correlation coefficient is: ", x)
 
 tutconf = os.path.join(os.path.dirname(__file__), 'tutorial.conf')
 
