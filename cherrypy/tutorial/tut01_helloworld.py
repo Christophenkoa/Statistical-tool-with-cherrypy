6797fa34a822d3cf28cbf1052a459a676281a03f
Nkoa Christophe <nkoachristophe@gmail.com>
2020-07-18 06:42:19 +0100

    remove unused variables

--- cherrypy/tutorial/tut01_helloworld.py
+++ cherrypy/tutorial/tut01_helloworld.py
@@ -112,12 +112,6 @@ class HelloWorld:
         # list 2 
         b = [10, 14, 12, 15, 20] 
         
-        # storing average of a 
-        av_a = sum(a)/len(a) 
-        
-        # storing average of b 
-        av_b = sum(b)/len(b) 
-        
         # making series from list a 
         a = pd.Series(a) 
         
